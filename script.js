24f7a1d567cb524ba5b144f0284b5ae821ef9465
Ju-Yu Goodwin <juyugoodwin@gmail.com>
2023-11-30 12:49:53 -0500

    Assignment 4 Completed

--- script.js
+++ script.js
@@ -4,102 +4,74 @@
  *
  */
 
-//
 // Variables
-//
+const inputBox = document.getElementById("item-name");
+const form = document.getElementById("form-results");
+const deletedCounter = document.createElement("p");
+let deletedCount = parseInt(localStorage.getItem("deletedCount")) || 0; // initialize the completed count
+let completedCount = parseInt(localStorage.getItem("completedCount")) || 0; // initialize the completed count
 
-var list = document.querySelector("ul");
-list.addEventListener("click", function(event) {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("checked");
-  }
-})
-
-let deletedChristmasItems = 0;
-let amountOfDeleted = document.querySelector("#number-deleted-items")
-
-// Constants
-const appID = 'app';
-
-const christmas = [];
-
-const christList = document.querySelector('.christmas-items');
-const christForm = document.querySelector('.add-item');
-const christName = document.querySelector('#item-name');
-
-
-// DOM Elements
-const appContainer = document.getElementById(appID);
-
-//
 // Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error('Error: Could not find app container');
-    return;
+function addTask (){
+  if(inputBox.value === ''){
+    alert("You must write something");
   }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    form.appendChild(li);
+    
+    let tempButton = document.createElement("button");
+    tempButton.textContent = "x";
+    li.appendChild(tempButton);
 
-  // Create an h1 and add it to our app
-
-  // Init complete
-  console.log('App successfully initialised');
-}
-
-function addChristmasItem(event) {
-  event.preventDefault();
-  const itemName = christName.value;
-  
-  christmas.push(itemName);
-  console.log(itemName);
-  renderList();
-  christForm.reset();
-}
-// Added things here
-// function toggleCheck(item) {
-  // item.classList.toggle("checked");
-// }
-
-function deleteChristmasItem(i) {
-  christmas.splice(i, 1);
-  deletedChristmasItems ++;
-  renderList();
-}
-
-function renderList() {
-  while (christList.firstChild) {
-    christList.removeChild(christList.firstChild);
+    li.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
   }
+  inputBox.value = "";
+  saveData();
+}
 
-  for (let i = 0; i < christmas.length; i++) {
-    const listItem = document.createElement('li');
-    listItem.textContent = christmas[i];
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'x';
-    deleteButton.addEventListener('click', () => {
-      deleteChristmasItem(i);
-    });
-
-    listItem.appendChild(deleteButton);
-
-    if (i === christmas.length - 1) {
-      listItem.classList.add('new-item-annimate');
+// Check mark
+form.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    if (e.target.classList.contains("checked")) {
+      completedCount++; // increment the completed count
+    } else {
+      completedCount--; // decrement the completed count
     }
-
-    amountOfDeleted.textContent = ("Amount of deleted items: "+ deletedChristmasItems);
-
-    christList.appendChild(listItem);
+    saveData();
   }
+  else if(e.target.tagName === "BUTTON"){
+    e.target.parentElement.remove();
+    deletedCount++;
+    saveData();
+  }
+}, false);
+
+// Update the completed count when an item is unchecked
+form.addEventListener("transitionend", function(e) {
+  if (e.target.tagName === "LI" && !e.target.classList.contains("checked")) {
+    completedCount--; // decrement the completed count
+    saveData();
+  }
+});
+
+// Update the completed count every second
+setInterval(function() {
+  document.getElementById("completed-count").innerHTML = `Total Completed items: ${completedCount}`;
+}, 500);
+
+setInterval(function() {
+  document.getElementById("deleted-count").innerHTML = `Total Deleted items: ${deletedCount}`;
+}, 500);
+
+function saveData(){
+  localStorage.setItem("data", form.innerHTML);
+  localStorage.setItem("completedCount", completedCount);
+  localStorage.setItem("deletedCount", deletedCount);
 }
-//
-// Inits & Event Listeners
-//
-inititialise();
-
-christForm.addEventListener('submit', addChristmasItem);
-
-renderList();
+function showData(){
+  form.innerHTML = localStorage.getItem("data");
+}
+showData();
